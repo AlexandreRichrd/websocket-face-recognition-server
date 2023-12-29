3c6e69f6a23bb471bfad1fe3df107330a9eaa91f
Alexandre Richard <86735269+AlexandreRichrd@users.noreply.github.com>
2023-12-29 17:26:23 +0100

    projet fonctionnel

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import json
 import base64
 
 
+from face_reco import generate_face_encodings, compare_encodings
+
+
 async def echo(websocket, path):
     async for message in websocket:
         # Déterminer si le message est de type bytes (données binaires)
@@ -13,17 +16,28 @@ async def echo(websocket, path):
             # Créer un BytesIO à partir des données binaires
             image_data = BytesIO(message)
 
-
-
             # Définir le chemin du fichier où l'image sera enregistrée
-            file_path = os.path.join('models', 'received_image.jpg')
+            file_path = os.path.join('encodings/queue', 'received_image.jpg')
 
             # Enregistrer l'image
             with open(file_path, 'wb') as file:
                 file.write(image_data.getbuffer())
 
             print(f"Image enregistrée sous : {file_path}")
-            await websocket.send(f"Image reçue et enregistrée sous : {file_path}")
+
+            # Générer les encodages de visage
+            generate_face_encodings(file_path, 'encodings/')
+
+            # Comparer les encodages
+            name = await compare_encodings()
+
+            print(name)
+
+            # Envoyer le résultat de la comparaison
+            await websocket.send(name)
+
+
+        # Déterminer si le message est de type str (chaîne de caractères)
         elif isinstance(message, str):
             data = json.loads(message)
             image_data = base64.b64decode(data['image'].split(',')[1])
@@ -38,7 +52,10 @@ async def echo(websocket, path):
                 file.write(image_data)
 
             print(f"Image enregistrée sous : {file_path}")
-            await websocket.send(f"Image reçue et enregistrée sous : {file_path}")
+            await websocket.send(f"Image reçue et en cours et traîtée.")
+
+            # Générer les encodages de visage
+            generate_face_encodings(file_path, 'models/encodings/')
 
 
         else:
@@ -50,6 +67,10 @@ async def main():
     # Assurez-vous que le dossier 'models' existe
     if not os.path.exists('models'):
         os.makedirs('models')
+    if not os.path.exists('models/encodings'):
+        os.makedirs('models/encodings')
+    if not os.path.exists('encodings/queue'):
+        os.makedirs('encodings/queue')
 
     async with websockets.serve(echo, 'localhost', 8765):
         await asyncio.Future()  # run forever
